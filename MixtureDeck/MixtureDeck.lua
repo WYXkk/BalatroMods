8394177752e0f9058188a9be0da5eef32162e185
WYXkk <2068559520@qq.com>
2024-05-16 00:08:34 +0800

    Update MixtureDeck.lua

--- MixtureDeck/MixtureDeck.lua
+++ MixtureDeck/MixtureDeck.lua
@@ -50,13 +50,13 @@ function Back:trigger_effect(args)
 	if self.name == 'Mixture Deck' then
 		if args.context == 'eval' and G.GAME.last_blind and G.GAME.last_blind.boss then
 			G.E_MANAGER:add_event(Event({
-	            func = (function()
-	                add_tag(Tag('tag_double'))
-	                play_sound('generic1', 0.9 + math.random()*0.1, 0.8)
-	                play_sound('holo1', 1.2 + math.random()*0.1, 0.4)
-	                return true
-	            end)
-	        }))
+					func = (function()
+						add_tag(Tag('tag_double'))
+						play_sound('generic1', 0.9 + math.random()*0.1, 0.8)
+						play_sound('holo1', 1.2 + math.random()*0.1, 0.4)
+						return true
+					end)
+			}))
 		end
 
 		if args.context == 'blind_amount' then
@@ -64,52 +64,52 @@ function Back:trigger_effect(args)
 		end
 
 		if args.context == 'final_scoring_step' then
-	        local tot = args.chips + args.mult
-	        args.chips = math.floor(tot/2)
-	        args.mult = math.floor(tot/2)
-	        update_hand_text({delay = 0}, {mult = args.mult, chips = args.chips})
+			local tot = args.chips + args.mult
+			args.chips = math.floor(tot/2)
+			args.mult = math.floor(tot/2)
+			update_hand_text({delay = 0}, {mult = args.mult, chips = args.chips})
 
-	        G.E_MANAGER:add_event(Event({
-	            func = (function()
-	                local text = localize('k_balanced')
-	                play_sound('gong', 0.94, 0.3)
-	                play_sound('gong', 0.94*1.5, 0.2)
-	                play_sound('tarot1', 1.5)
-	                ease_colour(G.C.UI_CHIPS, {0.8, 0.45, 0.85, 1})
-	                ease_colour(G.C.UI_MULT, {0.8, 0.45, 0.85, 1})
-	                attention_text({
-	                    scale = 1.4, text = text, hold = 2, align = 'cm', offset = {x = 0,y = -2.7},major = G.play
-	                })
-	                G.E_MANAGER:add_event(Event({
-	                    trigger = 'after',
-	                    blockable = false,
-	                    blocking = false,
-	                    delay =  4.3,
-	                    func = (function() 
-	                            ease_colour(G.C.UI_CHIPS, G.C.BLUE, 2)
-	                            ease_colour(G.C.UI_MULT, G.C.RED, 2)
-	                        return true
-	                    end)
-	                }))
-	                G.E_MANAGER:add_event(Event({
-	                    trigger = 'after',
-	                    blockable = false,
-	                    blocking = false,
-	                    no_delete = true,
-	                    delay =  6.3,
-	                    func = (function() 
-	                        G.C.UI_CHIPS[1], G.C.UI_CHIPS[2], G.C.UI_CHIPS[3], G.C.UI_CHIPS[4] = G.C.BLUE[1], G.C.BLUE[2], G.C.BLUE[3], G.C.BLUE[4]
-	                        G.C.UI_MULT[1], G.C.UI_MULT[2], G.C.UI_MULT[3], G.C.UI_MULT[4] = G.C.RED[1], G.C.RED[2], G.C.RED[3], G.C.RED[4]
-	                        return true
-	                    end)
-	                }))
-	                return true
-	            end)
-	        }))
+			G.E_MANAGER:add_event(Event({
+					func = (function()
+						local text = localize('k_balanced')
+						play_sound('gong', 0.94, 0.3)
+						play_sound('gong', 0.94*1.5, 0.2)
+						play_sound('tarot1', 1.5)
+						ease_colour(G.C.UI_CHIPS, {0.8, 0.45, 0.85, 1})
+						ease_colour(G.C.UI_MULT, {0.8, 0.45, 0.85, 1})
+						attention_text({
+							scale = 1.4, text = text, hold = 2, align = 'cm', offset = {x = 0,y = -2.7},major = G.play
+						})
+						G.E_MANAGER:add_event(Event({
+							trigger = 'after',
+							blockable = false,
+							blocking = false,
+							delay =  4.3,
+							func = (function() 
+										ease_colour(G.C.UI_CHIPS, G.C.BLUE, 2)
+										ease_colour(G.C.UI_MULT, G.C.RED, 2)
+									return true
+							end)
+						}))
+						G.E_MANAGER:add_event(Event({
+							trigger = 'after',
+							blockable = false,
+							blocking = false,
+							no_delete = true,
+							delay =  6.3,
+							func = (function() 
+									G.C.UI_CHIPS[1], G.C.UI_CHIPS[2], G.C.UI_CHIPS[3], G.C.UI_CHIPS[4] = G.C.BLUE[1], G.C.BLUE[2], G.C.BLUE[3], G.C.BLUE[4]
+									G.C.UI_MULT[1], G.C.UI_MULT[2], G.C.UI_MULT[3], G.C.UI_MULT[4] = G.C.RED[1], G.C.RED[2], G.C.RED[3], G.C.RED[4]
+									return true
+							end)
+						}))
+						return true
+					end)
+			}))
 
-	        delay(0.6)
-	        return args.chips, args.mult
-	    end
+			delay(0.6)
+			return args.chips, args.mult
+		end
 	end
 end
 
@@ -117,9 +117,9 @@ local Backapply_to_runRef = Back.apply_to_run
 function Back.apply_to_run(arg)
 	Backapply_to_runRef(arg)
 
-    if arg.effect.config.randomize_rank_suit_mixture then
-    	G.GAME.starting_params.no_faces = true
-    	G.GAME.mixture_deck_dynamic_ante_scaling = 1
+	if arg.effect.config.randomize_rank_suit_mixture then
+		G.GAME.starting_params.no_faces = true
+		G.GAME.mixture_deck_dynamic_ante_scaling = 1
 		G.E_MANAGER:add_event(Event({
 			func = function()
 				local pool={}
@@ -137,7 +137,7 @@ function Back.apply_to_run(arg)
 				return true
 			end
 		}))
-    end
+	end
 end
 
 local old_get_blind_amount=get_blind_amount
@@ -147,4 +147,24 @@ function get_blind_amount(ante)
 		v=v*(ante+1)*(1.2^ante)
 	end
 	return v
+end
+
+if DV and DV.SIM then
+	local oldbalance=DV.SIM.simulate_deck_effects
+	function DV.SIM.simulate_deck_effects()
+		if G.GAME.selected_back.name == 'Mixture Deck' then
+			local function plasma(data)
+				local sum = data.chips + data.mult
+				local half_sum = math.floor(sum/2)
+				data.chips = mod_chips(half_sum)
+				data.mult = mod_mult(half_sum)
+			end
+
+			plasma(DV.SIM.running.min)
+			plasma(DV.SIM.running.exact)
+			plasma(DV.SIM.running.max)
+		else
+			oldbalance()
+		end
+	end
 end
